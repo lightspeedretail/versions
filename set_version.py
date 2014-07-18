464cfc9d1f987d3ec3ca23d5e6cf96f20da8fa9d
Tyler Tidman <tyler.tidman@lightspeedretail.com>
2014-07-18 14:11:10 -0400

    LCO-172:  Make sure we can set updater product tag values

--- set_version.py
+++ set_version.py
@@ -2,31 +2,31 @@
 # -*- coding: utf-8 -*-
 # Run this script with a '-h' or '--help' option to get usage and description.
 
-usage = 'Usage:  %prog [options]'
+usage = '''Usage:  %prog [options]'''
 
 description = '''
 This script will sanitize json input while helping to set values.
 '''
 
 products = [
-    'bronze', 
-    'cloud', 
-    'updater', 
-    'webstore',
+    'bronze',
+    'cloud',
+    'reporting',
+    'updater',
     'webclient',
-    'reporting'
+    'webstore',
 ]
 environments = [
-    'pre-prod', 
-    'production', 
-    'staging', 
+    'cloud',
+    'firstwave',
+    'pre-prod',
+    'production',
+    'rad',
+    'radcloud',
+    'shop',
+    'staging',
+    'staging-firstwave',
     'staging-legacy',
-    'staging-firstwave', 
-    'cloud', 
-    'firstwave', 
-    'rad', 
-    'radcloud', 
-    'shop'
 ]
 
 from optparse import OptionParser
@@ -37,6 +37,8 @@ import sys
 
 def update_versions(infile, outfile, product, environment, version,
                     comment):
+    '''Set specific JSON version paylod values'''
+
     versions = _get_json_from_file(infile)
 
     # Set specific values in the dictionary
@@ -53,27 +55,33 @@ def update_versions(infile, outfile, product, environment, version,
 
 def _get_json_from_file(filename):
     '''Parse a json file to get a dictionary'''
+
     with open(filename, 'r') as filehandle:
         versions = json.load(filehandle)
     return versions
 
 
 def get_version(infile, product, environment):
-    ''' Parse a json file to get a dictionary '''
+    '''Parse a json file to get a dictionary'''
+
     versions = _get_json_from_file(infile)
 
     return versions['products'][product]['environments'][environment]['version']
 
 
 def parse_args(argv):
+    '''Process command-line arguments'''
+
     parser = OptionParser(usage=usage, description=description)
 
     parser.add_option('-i', '--infile', dest='infile',
-                      type='string', nargs=1, action='store', default='versions.json',
+                      type='string', nargs=1, action='store',
+                      default='versions.json',
                       help='(OPTIONAL) JSON input file to process')
 
     parser.add_option('-o', '--outfile', dest='outfile',
-                      type='string', nargs=1, action='store', default='versions.json',
+                      type='string', nargs=1, action='store',
+                      default='versions.json',
                       help='(OPTIONAL) JSON output file to process')
 
     parser.add_option('-p', '--product', dest='product',
@@ -122,10 +130,12 @@ def parse_args(argv):
 
 
 def do_work(options):
-    ''' Fire off the version and comment string stuff '''
+    '''Fire off the version and comment string stuff'''
+
     if options.get == 0:
         update_versions(infile=options.infile, outfile=options.outfile,
-                        product=options.product, environment=options.environment,
+                        product=options.product,
+                        environment=options.environment,
                         version=options.version, comment=options.comment)
     else:
         print get_version(infile=options.infile, product=options.product,
@@ -134,11 +144,13 @@ def do_work(options):
 
 def main(argv=None):
     '''main() flow.  Parse arguments and execute appropriate actions'''
+
     if argv is None:
         argv = sys.argv
 
     (options, args) = parse_args(argv)
     do_work(options)
+
 
 if __name__ == '__main__':
     sys.exit(main())
